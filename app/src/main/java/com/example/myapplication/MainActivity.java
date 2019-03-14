1162942c3ee72e64f8ae283f51100c30a99023b0
Alpen Adria Universität Klagenfurt <corinnagr@edu.aau.at>
2019-03-14 21:59:19 +0100

    declaired views from layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSendToServer = findViewById(R.id.btnSendToServer);
+        Button btnSortMatNumber = findViewById(R.id.btnSortMatNumber);
+
+        final EditText etxtMatNumber = findViewById(R.id.etxtMatNumber);
+
+        final TextView txtvSortedMatNumber = findViewById(R.id.txtvSortedMatNumber);
     }
 }
